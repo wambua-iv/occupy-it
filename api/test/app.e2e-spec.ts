66ff303ab47a1c7bf0c10e227e32b74d4dd82f2b
Musalu Wambua <musaluwambua@gmail.com>
2022-06-16 12:06:09 +0300

    authentication tests

--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -1,24 +1,70 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as pactum from 'pactum';
 import { AppModule } from './../src/app.module';
+import { Connection } from 'mongoose';
+import { DatabaseService } from '@/database/database.service';
+import { SignInAuthDto, SignUpAuthDto } from '@/auth/dto';
+import { CreatePropertyDto, PaymentDto, PropertyTypeDto, TenantDto } from '@/properties/dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let dbConnection: Connection;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+    if (process.env.NODE_ENV == 'test') {
+      app.listen(3080);
+    }
+    dbConnection = moduleFixture.get<DatabaseService>(DatabaseService).getDbHandle();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    dbConnection.collection('properties').deleteMany({});
+    dbConnection.collection('users').deleteMany({});
+    app.close();
+  });
+
+  describe('Authentication', () => {
+    describe('SignUp', () => {
+      const signUpDto: SignUpAuthDto = {
+        email: 'messy@gmail.com',
+        firstname: 'messy',
+        lastname: 'wambua',
+        ID: 36430147,
+        'phone number': 254728455643,
+        password: 'messyHunter',
+      };
+
+      it('should signUp', () => {
+        return pactum.spec().post('http://localhost:3080/auth/signup').withBody(signUpDto).expectStatus(200).inspect();
+      });
+    });
+
+    describe('SignIn', () => {
+      const signInDto: SignInAuthDto = {
+        email: 'messy@gmail.com',
+        password: 'messyHunter',
+      };
+
+      it('should sign', () => {
+        return pactum
+          .spec()
+          .post('http://localhost:3080/auth/signin')
+          .withBody(signInDto)
+          .expectStatus(200)
+          .stores('userAt', 'access_token');
+      });
+    });
   });
 });
