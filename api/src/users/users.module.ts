f44dc38527d5a71ad48a123e0331f580fa9b66c2
Musalu Wambua <musaluwambua@gmail.com>
2022-06-16 12:15:27 +0300

    intialize occupyIt

--- api/src/users/users.module.ts
+++ api/src/users/users.module.ts
@@ -1,8 +1,11 @@
+import { Users, UserSchema } from '@/models/users.models';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Users.name, schema: UserSchema }]), Users],
   controllers: [UserController],
   providers: [UsersService],
 })
