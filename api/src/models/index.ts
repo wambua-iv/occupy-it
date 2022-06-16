3c37d6be5b5a552fb71a82d72c28fae646b1fe55
Musalu Wambua <musaluwambua@gmail.com>
2022-06-16 12:11:24 +0300

    ft-added-payments-model

---
+++ api/src/models/index.ts
@@ -0,0 +1,3 @@
+export { Payments, PaymentDocument } from './payments.models';
+export { Properties, PropertiesDocument } from './properties.models';
+export { UserDocument, Users } from './users.models';
--- api/src/models/properties.models.ts
+++ api/src/models/properties.models.ts
@@ -6,7 +6,25 @@ export type PropertiesDocument = Properties & Document;
 @Schema({ timestamps: true })
 export class Properties {
   @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
   type: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  location: string;
+
+  @Prop({ required: true })
+  price: string;
+
+  @Prop({ type: Array, default: null })
+  amenities: [];
+
+  @Prop({ type: Array })
+  tenants: [];
 }
 
 export const PropertiesSchema = SchemaFactory.createForClass(Properties);
